e6f8808881328becc4f0e5012c95225a31dad195
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-06-26 20:41:23 +0200

    removed all files

--- src/lib/Store.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts" context="module">
-    import { writable } from 'svelte/store';
-    export const userAuth = writable(false);
-    export const loading = writable(true);
-
-</script>
--- src/routes/login/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { userAuth, loading } from '$lib/Store.svelte';
-    
-    import AnilistLogin from "$lib/AnilistLogin/AnilistLogin.svelte";
-import { onMount } from 'svelte';
-loading.set(false)
-</script>
-
-<AnilistLogin></AnilistLogin>
