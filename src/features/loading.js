ffd9cfaa87bef959ee9c4bbfd6843bc80fea1df5
Savio Andrade <andradesavio9073@gmail.com>
2022-06-28 17:09:16 +0200

    added recommended

--- src/features/loading.js
+++ src/features/loading.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const loadingSlice = createSlice({
     name: "loading",
     initialState: {
-        value: false,
+        value: true,
         
     },
     reducers: {
