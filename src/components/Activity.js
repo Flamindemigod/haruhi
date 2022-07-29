af620fba1ab46c7e8983adbc857e38e6934557af
Savio Andrade <andradesavio9073@gmail.com>
2022-07-29 06:55:09 +0200

    added rending and user activity

---
+++ src/components/Activity.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useSelector } from 'react-redux';
+import makeQuery from '../misc/makeQuery';
+import { Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { animated, useSpring } from '@react-spring/web'
+
+
+//https://gist.github.com/techtheory/383ad87b1fdfb36cde15
+function timeSince(date) {
+  var seconds = Math.floor(((new Date().getTime()/1000) - date)),
+  interval = Math.floor(seconds / 31536000);
+  
+  if (interval > 1) return interval + ` years ago`;
+  
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) return interval + " months ago";
+  
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) return interval + " days ago";
+  
+  interval = Math.floor(seconds / 3600); 
+  if (interval >= 1) return interval + " hours ago";
+  
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) return interval + " minutes ago";
+  
+  return Math.floor(seconds) + " seconds ago";
+}
+
+  const Activity = ({setIsEmpty}) => {
+    const [activity, setActivity] = useState([]);
+    let user = useSelector((state) => state.user.value);
+    useEffect(() => {
+        const getActivity = async () => {
+              const query = `query getMediaTrend {
+                  Page(perPage: 25) {
+                    pageInfo {
+                      hasNextPage
+                    }
+                    activities(type: ANIME_LIST, isFollowing: true, sort: ID_DESC) {
+                      ... on ListActivity {
+                        createdAt
+                        id
+                        user {
+                          name
+                          avatar{
+                              medium
+                          }
+                        }
+                        status
+                        progress
+                        media {
+                          id
+                          title {
+                            userPreferred
+                          }
+                          coverImage {
+                            medium
+                          }
+                        }
+                      }
+                    }
+                  }
+                }`
+            const variables = {}
+            const data = await makeQuery(query, variables)
+            setActivity(data.data.Page.activities)
+
+        }
+        getActivity();
+    }, [])
+    useEffect(()=>{if (activity.length){
+      setIsEmpty(false)
+      return;
+    }
+    setIsEmpty(true)
+
+  }, [activity])
+
+  const styles = useSpring({
+    from: {
+      opacity: 0,
+      translateX: "50%"
+    },
+    to: {
+      opacity: 1,
+      translateX: "0%"
+    },
+  })  
+
+    return (
+        <>
+            <div className='flex flex-col gap-4 p-4'>
+                {activity.map((data) => (
+                    <animated.div className='flex justify-start gap-4 p-4 bg-offWhite-500' style={{"--tw-bg-opacity": "0.6"}}>
+                        <LazyLoadImage src={data.media.coverImage.medium}></LazyLoadImage>
+
+                        <div className="flex flex-col justify-center gap-4">
+                            <div className='text-md'>{`${data.user.name} ${data.status} ${data.progress ? data.progress : ""} ${data.progress ? "of" : ""}`} <Link className='text-primary-500 hover:text-primary-300' to={`/anime/${data.media.id}`}> {`${data.media.title.userPreferred}`}</Link></div>
+                            <Avatar alt={`Avatar of user ${data.user.name}`} src={data.user.avatar.medium} />
+                            <div className='text-sm'>{ timeSince(data.createdAt)}</div>
+                        </div>
+
+                    </animated.div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Activity
---
+++ src/components/TrendingSeason.js
@@ -0,0 +1,118 @@
+import { useState, useEffect } from "react";
+import makeQuery from "../misc/makeQuery";
+import { useDispatch, useSelector } from 'react-redux';
+import { setLoading } from "../features/loading"
+import { Link } from 'react-router-dom';
+import AnimeCard from './AnimeCard';
+
+
+
+
+const TrendingSeason = () => {
+  const dispatch = useDispatch()
+    const [animeArray, setAnimeArray] = useState([]);
+    let user = useSelector((state) => state.user.value);
+
+    const getCurrentSeason = () => {
+      let currentTime = new Date();
+      let currentYear = currentTime.getUTCFullYear();
+      let currentMonth = currentTime.getMonth();
+  
+      if (currentMonth >= 2 && currentMonth < 4) {
+        return ["SPRING", currentYear]
+      }
+      else if (currentMonth >= 4 && currentMonth < 7){
+        return ["SUMMER", currentYear]
+        
+      }
+      else if (currentMonth >= 7 && currentMonth < 10){
+        return ["FALL", currentYear]
+
+      }
+      else{
+        if(currentMonth>=10){
+          return ["WINTER", currentYear]
+        }
+        else{
+          return ["WINTER", currentYear-1]
+        }
+      }
+    }
+    useEffect(() => {
+        const getCurrentAiring = async () => {
+            dispatch(setLoading(true))
+            var query = `
+            query getMediaTrend($season: MediaSeason, $seasonYear: Int) {
+              Page(perPage: 50){
+                pageInfo{
+                  hasNextPage
+                }
+                media(sort:[POPULARITY_DESC], season: $season, seasonYear: $seasonYear, type:ANIME) {
+                  id
+                  title{
+                    userPreferred
+                  }
+                  coverImage{
+                    large
+                  }
+                  airingSchedule {
+                    edges {
+                      node {
+                        airingAt
+                        timeUntilAiring
+                        episode
+                      }
+                    }
+                  }
+                }
+              }
+              }
+        `;
+
+              let season=null;
+              let seasonYear=null;
+            [season, seasonYear] = getCurrentSeason();
+              
+            var variables = {
+            season,
+            seasonYear
+            };
+
+            const getAiring = (media) => {
+              const airingSchedule = media.airingSchedule;
+              delete media.airingSchedule;
+              const nextAiringIndex = airingSchedule.edges.findIndex(
+                (element) => element.node.timeUntilAiring > 0
+              );
+              media["nextAiring"] = airingSchedule.edges[nextAiringIndex];
+              return media
+            }
+
+            let airingArrayAccumalated = []
+            let data;
+            variables["page"] = variables["page"] + 1
+            data = await makeQuery(query, variables);
+            for (const media in data.data.Page.media){
+              data.data.Page.media[media] = getAiring(data.data.Page.media[media])
+            }
+            airingArrayAccumalated = airingArrayAccumalated.concat(data.data.Page.media)
+            setAnimeArray(airingArrayAccumalated)
+            dispatch(setLoading(false))
+
+        };
+        getCurrentAiring();
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+
+        <><div className='text-2xl font-semibold p-2'>Trending this Season</div>
+
+            <div className=' flex flex-row gap-4 p-2  overflow-x-scroll styled-scrollbars'>
+                {animeArray.map((media) => (<Link className="cardLink" key={media.id} to={`/anime/${media.id}`}><AnimeCard mediaCover={media.coverImage.large} mediaTitle={media.title.userPreferred} nextAiringEpisode={media.nextAiring ? media.nextAiring.node.episode : 0} timeUntilAiring={media.nextAiring ? media.nextAiring.node.timeUntilAiring : 0} episodes={media.episodes} progress={media.progress ? media.progress : 0}/></Link>))}
+            </div>
+        </>
+    )
+}
+
+export default TrendingSeason
