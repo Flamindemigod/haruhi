d18e92c58d0b7261da09ee5444647993b63695f7
Savio Andrade <andradesavio9073@gmail.com>
2022-07-29 06:54:25 +0200

    removed defaults to prevent double load

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ContinueWatching from '../components/ContinueWatching';
 import { useSelector } from 'react-redux';
 import Recommended from '../components/Recommended';
-
+import TrendingSeason from '../components/TrendingSeason';
+import Activity from '../components/Activity';
 
 const Home = () => {
   const user = useSelector((state) => state.user.value)
+  const [isEmpty, setIsEmpty] = useState(null) 
   useEffect(()=>{
     document.title = "Haruhi - Home";}, [])
   return (
@@ -14,10 +16,14 @@ const Home = () => {
         <div className="text-3xl">Welcome Back {user.userName}</div>
         <div className=""></div>
         <img className="homeImg" height="60px" src='./HomeSplash.jpg' alt="a girl saluting you" />
+        <div className={`activity ${isEmpty ? "empty": ""}`}>
+          <Activity setIsEmpty={setIsEmpty}/>
+        </div>
       </div>
       <div className=' w-11/12 containerHome'>
         <ContinueWatching></ContinueWatching>
         <Recommended></Recommended>
+        <TrendingSeason></TrendingSeason>
         </div>
     </main>
   )
