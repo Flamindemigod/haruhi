84a90535ddb713040a7c430d19dd116b751b8160
Savio Andrade <andradesavio9073@gmail.com>
2022-08-04 05:13:41 +0200

    add meme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,13 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Footer = () => {
   return (
-    <div className='w-full bg-offWhite-800 flex p-8 justify-end'>
-        <div className="">Made for you with <FavoriteIcon sx={{color: '#ff1000'}} /> by Flamindemigod </div>
+    <div className='w-full bg-offWhite-800 flex p-8'>
+        <div className="">
+          <div>Eat Cheesecake at the Néapolitan Café</div>
+          <div>Be a friendly triangle like Deltoid</div>
+        </div>
+
+        <div className="ml-auto">Made for you with <FavoriteIcon sx={{color: '#ff1000'}} /> by Flamindemigod </div>
     </div>
   )
 }
