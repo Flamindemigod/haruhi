1c8ba6654d743992439fa1bc752e58130cfb9707
Savio Andrade <andradesavio9073@gmail.com>
2022-08-04 18:39:32 +0200

    updated search to be in parity with anilists to satisfy #8

---
+++ src/components/Search.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Search } from '@mui/icons-material';
+import { Button, ButtonBase, InputBase } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import makeQuery from '../misc/makeQuery';
+import { Link } from 'react-router-dom';
+import SearchMedia from './SearchMedia';
+import SearchCharacter from './SearchCharacter';
+import SearchStudio from './SearchStudio';
+import SearchStaff from './SearchStaff';
+
+
+const SearchInput = styled(InputBase)(({ theme }) => ({
+  '&': { width: "100%", height: "100%" },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    color: "white",
+    backgroundColor: "#414141",
+    transition: theme.transitions.create([
+      'box-shadow',
+    ]),
+    // Use the system font instead of the default Roboto font.
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
+
+const SearchAnime = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  useEffect(() => {
+    const search = async () => {
+      const mediaQuery = `query Search($searchstring: String){
+                Page{
+                  media(search:$searchstring, type:ANIME, sort:[POPULARITY_DESC]) {
+                    id
+                    title{
+                      userPreferred
+                    }
+                    coverImage{medium}
+                  }
+                }
+              }
+              `;
+
+      const characterQuery = `query Search($searchstring: String) {
+                Page {
+                  characters(search: $searchstring, sort: [RELEVANCE]) {
+                    id
+                    name
+                    image {
+                      medium
+                    }
+                  }
+                }
+              }`;
+
+      const query = `query Search($searchstring: String){
+            Page{
+              media(search:$searchstring, type:ANIME, sort:[POPULARITY_DESC]) {
+                id
+                title{
+                  userPreferred
+                }
+                coverImage{medium}
+                
+              }
+            }
+          }
+          `;
+      const variables = {
+        searchstring: searchQuery,
+      };
+      const data = await makeQuery(query, variables);
+    }
+    search()
+
+  }, [])
+  return (
+    <>
+      <Button sx={{ color: "#fff", fontSize: "1.25rem", textTransform: "none", fontWeight: "normal", borderColor: "transparent", ":hover": { borderColor: "#fff" } }} variant='outlined' endIcon={<Search />} onClick={() => { setDialogOpen(true) }}> Search </Button>
+      <Dialog
+        sx={{ "& .MuiPaper-root": { background: "transparent", boxShadow: "none" } }}
+        className='styled-scrollbars'
+        maxWidth={"3xl"}
+        fullWidth={true}
+        open={dialogOpen}
+        onClose={() => {
+          setSearchQuery("");
+          setDialogOpen(false);
+        }}
+        aria-labelledby="search-dialog"
+        aria-describedby="search-dialog-description"
+        scroll={"body"}
+      >
+        <DialogTitle id="search-dialog-title" sx={{ backgroundColor: "#313131", color: "white" }}>
+          <div className='flex justify-center items-center gap-2'>
+            <SearchInput placeholder="Search..." variant='standard' onChange={(e) => { setSearchQuery(e.target.value) }} ></SearchInput>
+            <Search sx={{ color: '#fff', mr: 1, my: 0.5 }} />
+          </div>
+        </DialogTitle>
+        <DialogContent className='styled-scrollbars' sx={{ backgroundColor: "transparent", color: "white" }}>
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 max-w-8xl gap-8 gap-y-8 mx-auto mt-8'>
+            <SearchMedia searchString={searchQuery} setSearchQuery={setSearchQuery} setDialogOpen={setDialogOpen} />
+            <SearchCharacter searchString={searchQuery} setSearchQuery={setSearchQuery} setDialogOpen={setDialogOpen} />
+            <SearchStaff searchString={searchQuery} setSearchQuery={setSearchQuery} setDialogOpen={setDialogOpen} />
+            <SearchStudio searchString={searchQuery} setSearchQuery={setSearchQuery} setDialogOpen={setDialogOpen} />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+
+  )
+}
+
+export default SearchAnime
