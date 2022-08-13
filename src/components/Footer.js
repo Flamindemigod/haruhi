b67fb28ec0f13b93cde3274983fd58c320eb4e08
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-08-13 07:58:51 +0200

    added github link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
   return (
@@ -11,6 +12,7 @@ const Footer = () => {
         <div className='ml-auto'>
         <div>Made for you with <FavoriteIcon sx={{color: '#ff1000'}} /> by Flamindemigod </div>
         <a href={"https://ko-fi.com/flamindemigod"}><img className='w-40' src='https://storage.ko-fi.com/cdn/brandasset/kofi_button_blue.png' alt='' /></a>
+        <a href={"https://github.com/andradesavio9073/haruhi"}>Submit Bug Reports or Help contribute on Github <GitHubIcon  /> </a>
         </div>
     </div>
   )
