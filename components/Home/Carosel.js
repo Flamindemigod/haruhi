dd6cc683da6cad98954d19e9e1fbb381faee9e7d
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-09-08 17:48:59 +0200

    added trending

---
+++ components/Home/Carosel.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material"
+import { IconButton, Box } from "@mui/material"
+import { useRef } from "react"
+
+const Carosel = ({ width = "100vw", children }) => {
+    const caroselTrack = useRef();
+    const [showLeftButton, setShowLeftButton] = useState(false)
+    const [showRightButton, setShowRightButton] = useState(true)
+
+    const scrollBack = () => {
+        caroselTrack.current.scrollLeft -= (window.innerWidth / 2);
+
+    }
+    const scrollForward = () => {
+        caroselTrack.current.scrollLeft += (window.innerWidth / 2);
+
+    }
+    useEffect(() => {
+        caroselTrack.current.addEventListener("scroll", () => {
+            setShowLeftButton(true)
+            setShowRightButton(true)
+            if (caroselTrack.current.scrollLeft === 0) {
+                setShowLeftButton(false)
+            }
+            if (parseInt(caroselTrack.current.scrollLeft + 1) === (caroselTrack.current.scrollWidth - caroselTrack.current.clientWidth)) {
+                setShowRightButton(false)
+            }
+        })
+    }, [])
+    return (
+        <div className="relative isolate">
+            {showLeftButton && (<IconButton onClick={scrollBack} className="absolute top-1/2 left-4 z-20" sx={{ backgroundColor: "var(--clr-primary)", opacity: "0.6", "&:hover": { backgroundColor: "var(--clr-primary)", opacity: 1 }, transform: "translateY(-50%)" }}>
+                <ArrowBackIos />
+            </IconButton>)}
+            <Box width={width} className="scroll-smooth relative flex overflow-x-auto overflow-y-hidden gap-4" ref={caroselTrack}>
+                {children}
+            </Box>
+            {showRightButton && (<IconButton onClick={scrollForward} className="absolute top-1/2 right-4 z-20" sx={{ backgroundColor: "var(--clr-primary)", opacity: "0.6", "&:hover": { backgroundColor: "var(--clr-primary)", opacity: 1 }, transform: "translateY(-50%)" }}>
+                <ArrowForwardIos />
+            </IconButton>)}
+        </div>
+    )
+}
+
+export default Carosel
