d186a018c60c9c75c3b447692390786f99997bef
Savio Andrade <andradesavio9073@gmail.com>
2022-09-09 20:24:06 +0200

    bugfixes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const isProd = process.env.NODE_ENV === "production" ? true : false;
 
 export const AnilistClientID = 9465;
-export const SERVER = isProd ? "https://haruhi.flamindemigod.com" : "http://haruhi.flamindemigod.com:8080" 
+export const SERVER = isProd ? "https://haruhi.flamindemigod.com:3000" : "http://haruhi.flamindemigod.com:8080" 
