23ec01e763994f67ce17c20dc067662a59ddb062
Savio Andrade <andradesavio9073@gmail.com>
2022-09-10 20:59:15 +0200

    passed user token

--- components/Home/Activity.js
+++ components/Home/Activity.js
@@ -8,32 +8,32 @@ import Link from 'next/link';
 
 //https://gist.github.com/techtheory/383ad87b1fdfb36cde15
 function timeSince(date) {
-    var seconds = Math.floor(((new Date().getTime() / 1000) - date)),
-        interval = Math.floor(seconds / 31536000);
+  var seconds = Math.floor(((new Date().getTime() / 1000) - date)),
+    interval = Math.floor(seconds / 31536000);
 
-    if (interval > 1) return interval + ` years ago`;
+  if (interval > 1) return interval + ` years ago`;
 
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) return interval + " months ago";
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) return interval + " months ago";
 
-    interval = Math.floor(seconds / 86400);
-    if (interval >= 1) return interval + " days ago";
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) return interval + " days ago";
 
-    interval = Math.floor(seconds / 3600);
-    if (interval >= 1) return interval + " hours ago";
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) return interval + " hours ago";
 
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) return interval + " minutes ago";
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) return interval + " minutes ago";
 
-    return Math.floor(seconds) + " seconds ago";
+  return Math.floor(seconds) + " seconds ago";
 }
 
 const Activity = () => {
-    const [activity, setActivity] = useState([]);
-    let user = useSelector((state) => state.user.value);
-    useEffect(() => {
-        const getActivity = async () => {
-            const query = `query getMediaTrend {
+  const [activity, setActivity] = useState([]);
+  let user = useSelector((state) => state.user.value);
+  useEffect(() => {
+    const getActivity = async () => {
+      const query = `query getMediaTrend {
                   Page(perPage: 10) {
                     pageInfo {
                       hasNextPage
@@ -63,33 +63,33 @@ const Activity = () => {
                     }
                   }
                 }`
-            const variables = {}
-            const data = await makeQuery(query, variables)
-            setActivity(data.data.Page.activities)
-        }
-        getActivity();
-    }, [])
+      const variables = {}
+      const data = await makeQuery(query, variables, user.userToken)
+      setActivity(data.data.Page.activities)
+    }
+    getActivity();
+  }, [])
 
 
-    return (
-        <>
-            <div className='flex flex-col gap-2 p-2'>
-                {activity.map((data) => (
-                    <div key={data.media.id} className='flex justify-start gap-4 p-4 bg-offWhite-700 isolate' style={{ "--tw-bg-opacity": "0.6" }}>
-                        <div className='h-40 w-32 relative'>
-                            <Image layout='fill' src={data.media.coverImage.medium} className={"object-cover"}></Image>
-                        </div>
-                        <div className="flex flex-col justify-center gap-4">
-                            <div className='text-md'>{`${data.user.name} ${data.status} ${data.progress ? data.progress : ""} ${data.progress ? "of" : ""}`} <Link href={`/anime/${data.media.id}`}><span className="text-primary-500 hover:text-primary-300">{data.media.title.userPreferred}</span></Link></div>
-                            <Avatar alt={`Avatar of user ${data.user.name}`} src={data.user.avatar.medium} />
-                            <div className='text-sm'>{timeSince(data.createdAt)}</div>
-                        </div>
-
-                    </div>
-                ))}
+  return (
+    <>
+      <div className='flex flex-col gap-2 p-2'>
+        {activity.map((data) => (
+          <div key={data.media.id} className='flex justify-start gap-4 p-4 bg-offWhite-700 isolate' style={{ "--tw-bg-opacity": "0.6" }}>
+            <div className='h-40 w-32 relative'>
+              <Image layout='fill' src={data.media.coverImage.medium} className={"object-cover"}></Image>
             </div>
-        </>
-    )
+            <div className="flex flex-col justify-center gap-4">
+              <div className='text-md'>{`${data.user.name} ${data.status} ${data.progress ? data.progress : ""} ${data.progress ? "of" : ""}`} <Link href={`/anime/${data.media.id}`}><span className="text-primary-500 hover:text-primary-300">{data.media.title.userPreferred}</span></Link></div>
+              <Avatar alt={`Avatar of user ${data.user.name}`} src={data.user.avatar.medium} />
+              <div className='text-sm'>{timeSince(data.createdAt)}</div>
+            </div>
+
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Activity
