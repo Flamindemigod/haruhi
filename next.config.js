d85713e477c18a0c4a19a5639f7186dec1a74ec7
Savio Andrade <andradesavio9073@gmail.com>
2022-09-11 13:42:50 +0200

    added haruhi dev to whitelist

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["haruhi.flamindemigod.com", 'storage.ko-fi.com', "s4.anilist.co"],
+    domains: ["haruhi.flamindemigod.com", "haruhi-dev.flamindemigod.com", 'storage.ko-fi.com', "s4.anilist.co"],
   },
 }
 
