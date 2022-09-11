c5373a334cc582b0144bea71e414a0ef2f35f9dd
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-09-11 12:14:09 +0200

    added label

--- components/Anime/Characters.js
+++ components/Anime/Characters.js
@@ -5,34 +5,36 @@ import React from 'react'
 import Carosel from '../Carosel'
 
 const Characters = ({ characters }) => {
-    console.log(characters)
     return (
-        <Carosel>
-            <Box className="grid grid-flow-col grid-rows-2 gap-4" sx={{ gridTemplateColumns: "repeat(auto-fill, minMax(25rem,30rem))" }}>
-                {characters.map((character) => (
-                    <Card key={character.node.id} className="flex justify-between" sx={{ minWidth: "25rem", maxWidth: "30rem" }}>
-                        <Link href={`/character/${character.node.id}`}>
-                            <Box className='flex gap-2 cursor-pointer'>
-                                <Image width={80} height={128} src={character.node.image.large} alt={character.node.name.userPreferred} />
-                                <Box className="flex flex-col justify-around">
-                                    <div className="text-md">{character.node.name.userPreferred}</div>
-                                    <div className="text-md">{character.role}</div>
+        <>
+            <div className='text-xl'>Characters</div>
+            <Carosel width='95vw'>
+                <Box className="grid grid-flow-col grid-rows-2 gap-4" sx={{ gridTemplateColumns: "repeat(auto-fill, minMax(25rem,30rem))" }}>
+                    {characters.map((character) => (
+                        <Card key={character.node.id} className="flex justify-between" sx={{ minWidth: "25rem", maxWidth: "30rem" }}>
+                            <Link href={`/character/${character.node.id}`}>
+                                <Box className='flex gap-2 cursor-pointer'>
+                                    <Image width={80} height={128} src={character.node.image.large} alt={character.node.name.userPreferred} />
+                                    <Box className="flex flex-col justify-around">
+                                        <div className="text-md">{character.node.name.userPreferred}</div>
+                                        <div className="text-md">{character.role}</div>
+                                    </Box>
                                 </Box>
-                            </Box>
-                        </Link>
-                        {character.voiceActors.length && <Link href={`/staff/${character.voiceActors[0].id}`}>
-                            <Box className='flex gap-2 cursor-pointer text-end'>
-                                <Box className="flex flex-col justify-around">
-                                    <div className="text-md">{character.voiceActors[0].name.userPreferred}</div>
-                                    <div className="text-md">JAPANESE</div>
+                            </Link>
+                            {character.voiceActors.length && <Link href={`/staff/${character.voiceActors[0].id}`}>
+                                <Box className='flex gap-2 cursor-pointer text-end'>
+                                    <Box className="flex flex-col justify-around">
+                                        <div className="text-md">{character.voiceActors[0].name.userPreferred}</div>
+                                        <div className="text-md">JAPANESE</div>
+                                    </Box>
+                                    <Image width={80} height={128} src={character.voiceActors[0].image.large} alt={character.node.name.userPreferred} />
                                 </Box>
-                                <Image width={80} height={128} src={character.voiceActors[0].image.large} alt={character.node.name.userPreferred} />
-                            </Box>
-                        </Link>}
-                    </Card>
-                ))}
-            </Box>
-        </Carosel>
+                            </Link>}
+                        </Card>
+                    ))}
+                </Box>
+            </Carosel>
+        </>
     )
 }
 
