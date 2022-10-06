d49053aba91c095dae8e543bb24612dc9a1951fb
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-10-06 23:14:39 +0200

    added staff page

--- components/CardTwo.js
+++ components/CardTwo.js
@@ -1,35 +1,56 @@
-import { Box } from '@mui/material'
-import Image from 'next/future/image'
-import React from 'react'
-import Link from './Link'
-import { animated, useSpring } from '@react-spring/web';
+import { Box } from "@mui/material";
+import Image from "next/future/image";
+import React from "react";
+import Link from "./Link";
+import { motion } from "framer-motion";
 
-const CardTwo = ({ width, height, mainImage, subImage = null, title, subTitle, link, status, mediaListEntryStatus }) => {
-    const styles = useSpring({
-        from: {
-            opacity: 0,
-            translateY: "50%",
-            scale: 0
-        },
-        to: {
-            scale: 1,
-            opacity: 1,
-            translateY: "0%"
-        },
-    })
-    return (
-        <animated.div style={styles} >
-            <Box className='card' sx={{ width }}>
-                <Link href={link}>
-                    <Box>
-                        <Image className='object-cover' width={width} height={height} src={mainImage} alt={title} />
-                        {subImage ? <Image className="absolute bottom-0 right-0" width={width / 4} height={height / 4} src={subImage} alt={subTitle} /> : <></>}
-                    </Box>
-                    <div className='font-semibold'>{title}</div>
-                </Link>
-            </Box>
-        </animated.div>
-    )
-}
+const CardTwo = ({
+  width,
+  height,
+  mainImage,
+  subImage = null,
+  role,
+  title,
+  subTitle,
+  link,
+  status,
+  mediaListEntryStatus,
+}) => {
+  return (
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      exit={{ scale: 0, opacity: 0 }}
+    >
+      <Box className="card" sx={{ width }}>
+        <Link href={link}>
+          <Box className="relative">
+            <Image
+              className="object-cover"
+              width={width}
+              height={height}
+              src={mainImage}
+              alt={title}
+            />
+            {subImage ? (
+              <Image
+                className="absolute bottom-0 right-0"
+                width={width / 3}
+                height={height / 3}
+                src={subImage}
+                alt={subTitle}
+              />
+            ) : (
+              <></>
+            )}
+          </Box>
+          <div className="media--title | font-semibold">{title}</div>
+          <div className="uppercase">{role}</div>
+          <div className="media--title | text-sm">{subTitle}</div>
+        </Link>
+      </Box>
+    </motion.div>
+  );
+};
 
-export default CardTwo
+export default CardTwo;
