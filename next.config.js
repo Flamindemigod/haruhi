12661f49689081f44aac191fee8769deeaec61b1
Savio Andrade <andradesavio9073@gmail.com>
2022-10-10 13:45:49 +0200

    fixed bug with starting new show

--- next.config.js
+++ next.config.js
@@ -3,9 +3,14 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["haruhi.flamindemigod.com", "haruhi-dev.flamindemigod.com", "s4.anilist.co"],
+    domains: [
+      "haruhi.flamindemigod.com",
+      "haruhi-dev.flamindemigod.com",
+      "s4.anilist.co",
+      "136.243.175.33",
+    ],
   },
-  experimental: { images: { allowFutureImage: true } }
-}
+  experimental: { images: { allowFutureImage: true } },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
