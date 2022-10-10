7fca8016f95f4c72caa6ad38dfd454332704b519
Savio Andrade <andradesavio9073@gmail.com>
2022-10-10 10:28:20 +0100

    Update config.js
    updated production settings

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === "production" ? true : false;
-export const AnilistClientID = 9465;
-export const SERVER = isProd ? "https://haruhi-dev.flamindemigod.com" : "http://haruhi.flamindemigod.com:8080";
+export const AnilistClientID = 8343;
+export const SERVER = isProd ? "https://haruhi.flamindemigod.com" : "http://haruhi.flamindemigod.com:8080";
 export const VIDEOSERVER = "https://haruhi-backend.flamindemigod.com";
 export const MALSERVER = "https://api.jikan.moe/v4/anime"
