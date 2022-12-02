f900da445e10fa41f9c037ecac5a35ec6ffa2bc8
Savio Andrade <andradesavio9073@gmail.com>
2022-12-02 10:02:13 +0100

    fixed anime season search

--- getCurrentSeason.js
+++ getCurrentSeason.js
@@ -1,21 +1,45 @@
 const getCurrentSeason = () => {
   let currentTime = new Date();
   let currentYear = currentTime.getUTCFullYear();
-  let currentMonth = currentTime.getMonth();
 
-  if (currentMonth >= 2 && currentMonth < 4) {
-    return ["SPRING", currentYear];
-  } else if (currentMonth >= 4 && currentMonth < 9) {
-    return ["SUMMER", currentYear];
-  } else if (currentMonth >= 9 && currentMonth < 11) {
-    return ["FALL", currentYear];
-  } else {
-    if (currentMonth >= 11) {
-      return ["WINTER", currentYear];
-    } else {
-      return ["WINTER", currentYear - 1];
-    }
-  }
+  var seasonArray = [
+    {
+      name: "SPRING",
+      date: new Date(
+        currentTime.getFullYear(),
+        2,
+        currentTime.getFullYear() % 4 === 0 ? 19 : 20
+      ).getTime(),
+    },
+    {
+      name: "SUMMER",
+      date: new Date(
+        currentTime.getFullYear(),
+        5,
+        currentTime.getFullYear() % 4 === 0 ? 20 : 21
+      ).getTime(),
+    },
+    {
+      name: "FALL",
+      date: new Date(
+        currentTime.getFullYear(),
+        8,
+        currentTime.getFullYear() % 4 === 0 ? 22 : 23
+      ).getTime(),
+    },
+    {
+      name: "WINTER",
+      date: new Date(
+        currentTime.getFullYear(),
+        11,
+        currentTime.getFullYear() % 4 === 0 ? 20 : 21
+      ).getTime(),
+    },
+  ];
+  let season = seasonArray
+    .filter(({ date }) => date <= currentTime)
+    .slice(-1)[0] || { name: "WINTER" };
+  return [season.name, currentYear];
 };
 
 export default getCurrentSeason;
