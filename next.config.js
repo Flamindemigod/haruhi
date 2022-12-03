5b3f8dff97836ef7bd8ba3adf2631d20c5436195
Savio Andrade <andradesavio9073@gmail.com>
2022-12-03 18:16:22 +0100

    yeeted old v2 and hello next js 13 beta

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      "storage.ko-fi.com",
-      "haruhi.flamindemigod.com",
-      "haruhi-dev.flamindemigod.com",
-      "s4.anilist.co",
-      "136.243.175.33",
-    ],
+  experimental: {
+    appDir: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
