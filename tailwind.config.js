5d0c66f01b046b7b31ddd2d268c5e0d5fc09cef8
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-12-06 01:23:27 +0100

    reverted darkmode to user themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  // darkMode: "class",
   safelist: ["text-primary-500"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
