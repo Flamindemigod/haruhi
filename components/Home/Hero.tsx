9c5b4ec39d78711dff0cd243120a55133b348a7f
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-12-15 18:23:44 +0100

    added logo favicon and new font

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -2,8 +2,8 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="h-[60vh] flex flex-col justify-center gap-2 text-white p-12 bg-black bg-opacity-50 rounded-md w-full relative">
-      <h3 className="text-3xl">Welcome To Haruhi</h3>
-      <p className="">
+      <h3 className="text-3xl font-georama font-medium">Welcome To Haruhi</h3>
+      <p className="font-georama text-lg">
         A next gen anime and manga platform made for you with anilist
         integration to provide a seemless watching experience
       </p>
