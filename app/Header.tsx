9cf1933e8a46939e1f0ed9ec9d5c43e68c7edde4
Savio Andrade <andradesavio9073@gmail.com>
2022-12-19 23:12:55 +0100

    basic search done

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,13 +4,17 @@ import Avatar from "../primitives/Avatar";
 import { userContext } from "./UserContext";
 import SignIn from "../primitives/SignIn";
 import NavDrawer from "../primitives/NavDrawer";
+import SearchBasicDialog from "../components/SearchBasicDialog";
 
 const Header = () => {
   const user = useContext(userContext);
   return (
     <div className="flex relative py-2 px-8 justify-between">
       <NavDrawer />
-      {user.userAuth ? <Avatar /> : <SignIn />}
+      <div className="flex gap-4 items-center">
+        <SearchBasicDialog />
+        {user.userAuth ? <Avatar /> : <SignIn />}
+      </div>
     </div>
   );
 };
