29dd235892376fef8da97d203df6e3b7ce2dd630
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2022-12-26 17:48:45 +0100

    added more timeouts

--- app/FixLoading.tsx
+++ app/FixLoading.tsx
@@ -8,12 +8,13 @@ export default function FixLoading() {
   const router = useRouter();
 
   useEffect(() => {
-    const timeouts = [100].map((t) =>
+    const timeouts = [100, 500, 1000].map((t) =>
       setTimeout(() => router.replace(String(path)), t)
     );
     return () => {
       timeouts.forEach((t) => clearTimeout(t));
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [path]);
 
   return <></>;
