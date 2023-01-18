4f99e0d4d608860bd77bebed6c2af3c179b36d16
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-01-18 14:18:27 +0100

    reverted back to 13.1.1

--- app/FixLoading.tsx
+++ app/FixLoading.tsx
@@ -7,15 +7,15 @@ export default function FixLoading() {
   const path = usePathname();
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const timeouts = [100, 500, 1000].map((t) =>
-  //     setTimeout(() => router.replace(String(path)), t)
-  //   );
-  //   return () => {
-  //     timeouts.forEach((t) => clearTimeout(t));
-  //   };
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [path]);
+  useEffect(() => {
+    const timeouts = [100, 500, 1000, 2000].map((t) =>
+      setTimeout(() => router.replace(String(path)), t)
+    );
+    return () => {
+      timeouts.forEach((t) => clearTimeout(t));
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [path]);
 
   return <></>;
 }
