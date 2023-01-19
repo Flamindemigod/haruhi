8b029708acf1c2857b0ba6fed1581592c6848ae7
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-01-19 17:25:27 +0100

    removed console logs

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE || "";
-console.log(supabaseServiceKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   realtime: {
