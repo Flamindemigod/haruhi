a3c5fea7efa00a4ebd2987bc9b674bb19c1d3d42
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-02-24 12:00:08 +0100

    renabled fixloading till 13.2 port

---
+++ utils/useMediaSync.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { supabase } from "./supabaseClient";
+import { RealtimeChannel } from "@supabase/supabase-js";
+import _ from "lodash";
+
+const useMediaSync = (
+  syncCode: string,
+  validationFunction: (payload:any) => boolean,
+  syncPlaying: (payload: any) => void,
+  syncPlayed: (payload: any) => void,
+  syncSeekTo: (payload: any) => void,
+  syncEpisodes: (payload: any) => void
+) => {
+  const syncChannelRef = useRef<RealtimeChannel | null>(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  
+
+  useEffect(() => {
+    if (!syncCode) return;
+
+    syncChannelRef.current = supabase.channel(syncCode);
+    syncChannelRef.current
+      .on("broadcast", { event: "playing" }, (e)=>{if(validationFunction(e)){syncPlaying(e)}})
+      .on("broadcast", { event: "played" }, (e)=>{if(validationFunction(e)){syncPlayed(e)}})
+      .on("broadcast", { event: "seekTo" }, (e)=>{if(validationFunction(e)){syncSeekTo(e)}})
+      .on("broadcast", { event: "episodes" }, (e)=>{if(validationFunction(e)){syncEpisodes(e)}})
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(syncChannelRef.current!);
+      // syncChannelRef.current.unsubscribe();
+    };
+  }, []);
+  const dispatchSyncChannelRef = (props: any) => {
+    if (syncChannelRef.current) {
+      syncChannelRef.current.send({
+        type: "broadcast",
+        ...props,
+      });
+    }
+  };
+  return dispatchSyncChannelRef;
+};
+
+export default useMediaSync;
