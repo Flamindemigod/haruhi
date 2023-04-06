fa00e3f55cbdf1186be5e76795add3b153a5a329
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-04-06 21:14:49 +0200

    updated manga blacklist

--- pages/api/getMangaChapters.ts
+++ pages/api/getMangaChapters.ts
@@ -12,7 +12,13 @@ export default async function handler(
       ["Kaguya-sama: Love is War"]: "Kaguya-sama - Love Is War",
       ["Mushoku Tensei: Jobless Reincarnation"]:
         "Mushoku Tensei - Jobless Reincarnation",
-      ["[Oshi no Ko]"]: "Oshi no Ko",
+      ["Tokyo卍Revengers"]: "Tokyo Revengers",
+      [`S-Rank Monster no "Behemoth" dakedo, Neko to Machigawarete Elf Musume no Pet toshite Kurashitemasu`]:
+        "I Am Behemoth of the S Rank Monster but I Am Mistaken as a Cat and I Live as a Pet of Elf Girl",
+      ["Wanwan Monogatari: Kanemochi no Inu ni Shite to wa Itta ga, Fenrir ni Shiro to wa Itte Nee!"]:
+        "Bark Bark Story",
+
+      // ["[Oshi no Ko]"]: "Oshi no Ko",
     };
     const mangakakalot = new MANGA.MangaKakalot();
 
