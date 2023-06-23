6dabacc0942e3518a8c43168d3de0c6881f15cea
Savio Andrade <andradesavio9073@gmail.com>
2023-06-23 11:53:50 +0200

    switched to manga reader. added image descrambler. added shuffle toggle on page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /canvas\/build\/Release\/canvas\.node$/,
+      loader: "node-loader",
+    });
+
+    config.module.rules.push({
+      test: /\.(gif|png|jpe?g|svg|woff2?|ttf|eot)$/i,
+      use: "raw-loader",
+    });
+    return config;
+  },
   reactStrictMode: false,
   experimental: {
     appDir: true,
