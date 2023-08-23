d97dc4e41df59da81fd1c8e8c9db21957f1b914c
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-08-23 16:10:16 +0200

    updated consumet to fix gogo scraper

--- components/Anime/Streaming.typegen.ts
+++ components/Anime/Streaming.typegen.ts
@@ -1,49 +1,43 @@
-// This file was automatically generated. Edits will be overwritten
 
-export interface Typegen0 {
-  "@@xstate/typegen": true;
-  internalEvents: {
-    "": { type: "" };
-    "xstate.init": { type: "xstate.init" };
-  };
-  invokeSrcNameMap: {};
-  missingImplementations: {
-    actions: never;
-    delays: never;
-    guards: never;
-    services: never;
-  };
-  eventsCausingActions: {
-    increment: "";
-    incrementAndComplete: "";
-    initAndComplete: "";
-    initAndCompleteAndRewatch: "";
-    initAndWatching: "";
-    initAndWatchingAndRewatch: "";
-  };
-  eventsCausingDelays: {};
-  eventsCausingGuards: {
-    canUpdate: "Episode Threshold Reached";
-    isCompleted: "";
-    isLastEpisode: "";
-    isUserAuth: "";
-    isWatching: "";
-  };
-  eventsCausingServices: {};
-  matchesStates:
-    | "Awaiting Episode Change State"
-    | "Awaiting Episode Update Event"
-    | "Check Episode Status"
-    | "Check if is Last Episode 1"
-    | "Check if is Last Episode 2"
-    | "Check if is Last Episode 3"
-    | "Do Nothing"
-    | "Increment Episode"
-    | "Increment Episode and Set as Completed"
-    | "Initial state"
-    | "Set Episode as 1 and Set as Completed"
-    | "Set Episode as 1 and Set as Completed and Increment Rewatches"
-    | "Set Episode as 1 and Set as Watching"
-    | "Set Episode as 1 and Set as Watching and Increment Rewatches";
-  tags: never;
-}
+  // This file was automatically generated. Edits will be overwritten
+
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "": { type: "" };
+"xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          "increment": "";
+"incrementAndComplete": "";
+"initAndComplete": "";
+"initAndCompleteAndRewatch": "";
+"initAndWatching": "";
+"initAndWatchingAndRewatch": "";
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          "canUpdate": "Episode Threshold Reached";
+"isCompleted": "";
+"isLastEpisode": "";
+"isUserAuth": "";
+"isWatching": "";
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "Awaiting Episode Change State" | "Awaiting Episode Update Event" | "Check Episode Status" | "Check if is Last Episode 1" | "Check if is Last Episode 2" | "Check if is Last Episode 3" | "Do Nothing" | "Increment Episode" | "Increment Episode and Set as Completed" | "Initial state" | "Set Episode as 1 and Set as Completed" | "Set Episode as 1 and Set as Completed and Increment Rewatches" | "Set Episode as 1 and Set as Watching" | "Set Episode as 1 and Set as Watching and Increment Rewatches";
+        tags: never;
+      }
+  
