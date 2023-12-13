3742a7c8c59bf48b10dd728689ec85edee6c7196
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-12-13 16:22:02 +0100

    removed unauthed stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const sesh = await getServerAuthSession();
-  var user = await api.user.getUser.query();
-  if (!!sesh?.user) {
-    await api.user.refreshUser.mutate();
-  }
+  // const sesh = await getServerAuthSession();
+  // var user = await api.user.getUser.query();
+  // if (!!sesh?.user) {
+  // await api.user.refreshUser.mutate();
+  // }
   return (
     <main className="h-screen">
-      <div>Hello there {JSON.stringify(sesh?.user)}</div>
+      <div>Hello there</div>
+      {/* <div>Hello there {JSON.stringify(sesh?.user)}</div> */}
     </main>
   );
 }
