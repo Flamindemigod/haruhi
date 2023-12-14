712c6006d99c473dfc14a0453cc6d0b3d13a6a83
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-12-14 16:52:16 +0100

    rewritten avatar dropdown using dropdown primitive

--- src/app/_components/Avatar.tsx
+++ src/app/_components/Avatar.tsx
@@ -1,19 +1,58 @@
 "use client";
 
-import cx from "classix";
 import Image from "next/image";
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import Dropdown, { Props as DropdownProps } from "~/primitives/Dropdown";
+
+const dropdownProps: Omit<DropdownProps, "trigger"> = {
+  align: "end",
+  arrow: true,
+  sideOffset: 10,
+  content: [
+    {
+      type: "Item",
+      content: [
+        {
+          lable: "Settings",
+          content: (
+            <Link
+              className="flex items-center justify-center text-offWhite-700 dark:text-offWhite-100"
+              href={"/"}
+            >
+              Settings
+            </Link>
+          ),
+        },
+        {
+          lable: "Signout",
+          content: (
+            <button
+              className="flex w-full items-center justify-center text-red-700 dark:text-red-400"
+              onClick={() => {
+                signOut();
+              }}
+            >
+              Signout
+            </button>
+          ),
+        },
+      ],
+    },
+  ],
+};
+
 const Avatar = () => {
   const { data: session, status } = useSession();
+
   return (
-    <DropdownMenuPrimitive.Root>
-      <DropdownMenuPrimitive.Trigger asChild>
-        {status == "loading" ? (
+    <Dropdown
+      {...dropdownProps}
+      trigger={
+        status == "loading" ? (
           <div
             className={
-              "h-14 w-14 animate-pulse rounded-full bg-offWhite-900/50"
+              "h-14 w-14 flex-shrink-0 animate-pulse rounded-full bg-offWhite-900/50"
             }
           ></div>
         ) : (
@@ -26,64 +65,87 @@ const Avatar = () => {
               "cursor-pointer rounded-full object-cover hover:animate-spin hover:motion-reduce:animate-none"
             }
           />
-        )}
-      </DropdownMenuPrimitive.Trigger>
-      <DropdownMenuPrimitive.Portal>
-        <DropdownMenuPrimitive.Content
-          align="end"
-          sideOffset={5}
-          className={cx(
-            " radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
-            "w-48 rounded-lg px-1.5 py-1 shadow-md md:w-36",
-            "bg-white dark:bg-offWhite-800",
-          )}
-        >
-          <DropdownMenuPrimitive.Item
-            className={cx(
-              "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
-              "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
-            )}
-            asChild
-          >
-            <a href={"https://anilist.co/settings"}>
-              <span className="flex-grow text-center text-offWhite-700 dark:text-offWhite-100">
-                Anilist Settings
-              </span>
-            </a>
-          </DropdownMenuPrimitive.Item>
-          <DropdownMenuPrimitive.Item
-            className={cx(
-              "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
-              "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
-            )}
-            asChild
-          >
-            <Link href={"/prefrences"}>
-              <span className="flex-grow text-center text-offWhite-700 dark:text-offWhite-100">
-                Prefrences
-              </span>
-            </Link>
-          </DropdownMenuPrimitive.Item>
-          <DropdownMenuPrimitive.Item
-            className={cx(
-              "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
-              "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
-            )}
-            asChild
-          >
-            <button
-              onClick={() => {
-                signOut();
-              }}
-              className="flex w-full justify-center text-red-700 dark:text-red-400"
-            >
-              Logout
-            </button>
-          </DropdownMenuPrimitive.Item>
-        </DropdownMenuPrimitive.Content>
-      </DropdownMenuPrimitive.Portal>
-    </DropdownMenuPrimitive.Root>
+        )
+      }
+    />
   );
+  // return (
+  //   <DropdownMenuPrimitive.Root>
+  //     <DropdownMenuPrimitive.Trigger asChild>
+  //       {status == "loading" ? (
+  //         <div
+  //           className={
+  //             "h-14 w-14 animate-pulse rounded-full bg-offWhite-900/50"
+  //           }
+  //         ></div>
+  //       ) : (
+  //         <Image
+  //           src={session?.user.image || ""}
+  //           alt={`Avatar of ${session?.user.name}`}
+  //           width={56}
+  //           height={56}
+  //           className={
+  //             "cursor-pointer rounded-full object-cover hover:animate-spin hover:motion-reduce:animate-none"
+  //           }
+  //         />
+  //       )}
+  //     </DropdownMenuPrimitive.Trigger>
+  //     <DropdownMenuPrimitive.Portal>
+  //       <DropdownMenuPrimitive.Content
+  //         align="end"
+  //         sideOffset={5}
+  //         className={cx(
+  //           " radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
+  //           "w-48 rounded-lg px-1.5 py-1 shadow-md md:w-36",
+  //           "bg-white dark:bg-offWhite-800",
+  //         )}
+  //       >
+  //         <DropdownMenuPrimitive.Item
+  //           className={cx(
+  //             "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
+  //             "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
+  //           )}
+  //           asChild
+  //         >
+  //           <a href={"https://anilist.co/settings"}>
+  //             <span className="flex-grow text-center text-offWhite-700 dark:text-offWhite-100">
+  //               Anilist Settings
+  //             </span>
+  //           </a>
+  //         </DropdownMenuPrimitive.Item>
+  //         <DropdownMenuPrimitive.Item
+  //           className={cx(
+  //             "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
+  //             "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
+  //           )}
+  //           asChild
+  //         >
+  //           <Link href={"/prefrences"}>
+  //             <span className="flex-grow text-center text-offWhite-700 dark:text-offWhite-100">
+  //               Prefrences
+  //             </span>
+  //           </Link>
+  //         </DropdownMenuPrimitive.Item>
+  //         <DropdownMenuPrimitive.Item
+  //           className={cx(
+  //             "flex cursor-pointer select-none items-center rounded-md px-2 py-2 text-base outline-none",
+  //             "text-offWhite-400 hover:bg-offWhite-100 focus:bg-offWhite-100 dark:text-offWhite-500 dark:focus:bg-offWhite-900",
+  //           )}
+  //           asChild
+  //         >
+  //           <button
+  //             onClick={() => {
+  //               signOut();
+  //             }}
+  //             className="flex w-full justify-center text-red-700 dark:text-red-400"
+  //           >
+  //             Logout
+  //           </button>
+  //         </DropdownMenuPrimitive.Item>
+  //       </DropdownMenuPrimitive.Content>
+  //     </DropdownMenuPrimitive.Portal>
+  //   </DropdownMenuPrimitive.Root>
+  // );
 };
 
 export default Avatar;
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -6,19 +6,18 @@ import Avatar from "./Avatar";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import Link from "next/link";
 import { Navigation } from "./Navigation";
-import { Fragment, Suspense } from "react";
 
 export const Header = async () => {
   const session = await getServerAuthSession();
   return (
     <header
       className={cx(
-        "flex flex-row items-center justify-between py-4",
-        "animate-bg-travel-y bg-500% bg-gradient-to-bl from-primary-500 via-indigo-500 to-primary-500 ",
+        "flex flex-row items-center justify-between gap-2 px-2 py-4",
+        "animate-bg-travel-y bg-gradient-to-bl from-primary-500 via-indigo-500 to-primary-500 bg-500% ",
       )}
     >
       {/* Logo */}
-      <Link href={"/"} className="flex flex-row items-center px-2">
+      <Link href={"/"} className="flex flex-row items-center">
         <Image
           alt="Haruhi Logo"
           src={"./Logo.png"}
@@ -27,14 +26,11 @@ export const Header = async () => {
           priority
         />
       </Link>
-      <Suspense>
-        <Navigation isUserAuth={!!session?.user} />
-      </Suspense>
-      <div className="flex flex-row items-center gap-4 px-2">
-        <ThemeSwitcher />
-        {/* Sign In Button / User Avatar */}
-        {!session?.user ? <SignIn /> : <Avatar />}
-      </div>
+      <Navigation isUserAuth={!!session?.user} />
+      {/* Theme Switcher */}
+      <ThemeSwitcher />
+      {/* Sign In Button / User Avatar */}
+      {!session?.user ? <SignIn /> : <Avatar />}
     </header>
   );
 };
