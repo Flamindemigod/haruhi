8fd1515c2c22935ed6839e7050dec798dc3858b3
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2023-12-22 00:22:58 +0100

    some more styling

--- src/app/_components/SearchResult.Anime.tsx
+++ src/app/_components/SearchResult.Anime.tsx
@@ -10,10 +10,11 @@ export type Props = {
 
 export default (props: Props) => {
   return (
-    <div className="relative grid max-h-36 grid-cols-4 place-content-center">
+    <div className="group relative grid  grid-cols-4 place-content-center">
       {/* Image */}
-      <div className="relative  max-h-36 ">
+      <div className="relative  ">
         <Image
+          draggable={false}
           className="col-span-1"
           src={props.media.coverImage.medium!}
           alt={`Cover of ${props.media.title.userPreferred}`}
@@ -22,10 +23,25 @@ export default (props: Props) => {
         />
       </div>
       {/* Content */}
-      <div className="col-span-full col-start-2 grid grid-flow-col grid-rows-3 items-center">
+      <div className="col-span-full col-start-2 grid grid-flow-col grid-rows-[auto_1fr_auto] items-center">
         {/* Title */}
         <Marquee>{props.media.title.userPreferred}</Marquee>
         {/* Description */}
+        <div
+          className="line-clamp-4 h-full overflow-hidden p-1"
+          dangerouslySetInnerHTML={{ __html: props.media.description! }}
+        ></div>
+        {/* Genre Tags */}
+        <div className="flex h-14 gap-2 overflow-y-auto p-2">
+          {props.media.genres.map((g) => (
+            <div
+              key={g}
+              className="h-min rounded-lg p-1 group-odd:bg-primary-300"
+            >
+              {g}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
