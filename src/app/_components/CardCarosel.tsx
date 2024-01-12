324fc42ebc5c00598c07711e5fee0aad149730cc
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2024-01-12 20:38:39 +0100

    Added Proper fallbacks on suspense

--- src/app/_components/CardCarosel.tsx
+++ src/app/_components/CardCarosel.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Carosel from "~/primitives/Carosel";
 import Card from "./Card";
 import ViewSegment from "./ViewSegment";
+import { ThreeCircles } from "react-loader-spinner";
 
 type CardMedia = Pick<
   Media,
@@ -57,24 +58,41 @@ export const Segment = (props: SegmentProps) => (
   </ViewSegment>
 );
 
+export const SegmentFallback = (
+  props: Omit<SegmentProps, "data" | "type"> & { fallback?: string },
+) => (
+  <ViewSegment title={props.title}>
+    <div className="flex h-32 items-center  justify-center gap-4 py-2 sm:h-48 md:h-64">
+      <ThreeCircles
+        height="100%"
+        color="var(--clr-primary)"
+        wrapperClass="items-center justify-center flex-shrink p-2"
+        visible={true}
+        ariaLabel="three-circles-rotating"
+      />
+      <span className="p-2 text-lg font-medium text-primary-500 md:text-xl">
+        {props.fallback}
+      </span>
+    </div>
+  </ViewSegment>
+);
+
 const CardCarosel = (props: Props) => {
   const [reset, setReset] = useState<boolean>(false);
   return (
-    <>
-      <Carosel height={"clamp(150px, 100%, 210px)"}>
-        {props.data.map((d) => (
-          <Card
-            reset={reset}
-            onReset={() => {
-              setReset((state) => !state);
-            }}
-            data={d}
-            type={props.type}
-            key={d.id}
-          />
-        ))}
-      </Carosel>
-    </>
+    <Carosel height={"clamp(150px, 100%, 210px)"}>
+      {props.data.map((d) => (
+        <Card
+          reset={reset}
+          onReset={() => {
+            setReset((state) => !state);
+          }}
+          data={d}
+          type={props.type}
+          key={d.id}
+        />
+      ))}
+    </Carosel>
   );
 };
 
--- src/app/_components/Recommended/Recommended.tsx
+++ src/app/_components/Recommended/Recommended.tsx
@@ -1,20 +1,16 @@
-import { api } from "~/trpc/server";
-import { Category } from "~/types.shared/anilist";
-import { SegmentProps, Segment } from "../CardCarosel";
+import { SegmentFallback } from "../CardCarosel";
 import { Suspense } from "react";
 import RecommendedAnime from "./Recommended.Anime";
 import RecommendedManga from "./Recommended.Manga";
 
 export default async () => {
-  const data_recommended_manga = await api.anilist.getRecommendedManga.query();
   return (
     <div className="flex w-full flex-col gap-4 py-4">
       <Suspense
         fallback={
-          <Segment
+          <SegmentFallback
             title="Anime You Might Like"
-            data={[]}
-            type={Category.anime}
+            fallback="Building Anime Recommendations"
           />
         }
       >
@@ -22,10 +18,9 @@ export default async () => {
       </Suspense>
       <Suspense
         fallback={
-          <Segment
+          <SegmentFallback
             title="Manga You Might Like"
-            data={[]}
-            type={Category.manga}
+            fallback="Building Manga Recommendations"
           />
         }
       >
